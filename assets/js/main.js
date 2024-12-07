d3b4db1c4dbe5dc2af276d378604b9b2a3541488
Muhammad Qasim <malikqasim20051@gmail.com>
2024-12-07 16:40:36 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    effect: "fade",
+    simulateTouch: false,
+    touchStartPreventDefault: true,
+  });
 
+
+
+ 
