38ceff4376974f235909153ce04edb51a2cee0b2
Muhammad Qasim <malikqasim20051@gmail.com>
2024-12-09 05:36:03 +0500

    browse-locations-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,25 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  loop:true,
+  spaceBetween: 0,
+  // autoplay: {
+  //   delay: 4000,
+  //   disableOnInteraction: false,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+
+});
+
+
+
+
+
+
  
