f74ee03abf6c370982b8c90fb633b115a9d5d654
Muhammad Qasim <malikqasim20051@gmail.com>
2024-12-17 16:50:05 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,3 +129,58 @@ $(window).on('scroll', function() {
 });
 
 
+
+
+
+/* <div class="content-lists">
+                                <ul>
+                                    <li>
+                                        <div class="content-list-item">
+                                                <a href="#" class="link-hover">American Independence Museum</a>
+                                                <hr>
+                                                <small>8.2 / 10</small>
+                                        </div><!-- /.content-list-item -->
+                                    </li>  
+                                    <li>
+                                        <div class="content-list-item">
+                                                <a href="#" class="link-hover">Lorem, ipsum dolor.</a>
+                                                <hr>
+                                                <small>8.2 / 10</small>
+                                        </div><!-- /.content-list-item -->
+                                    </li>  
+                                   
+                                    
+                                </ul>
+                            </div><!-- /.content-lists -->  */
+
+
+
+
+
+
+
+
+
+
+
+
+                    //         <nav>
+                    //     <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    //         <button class="nav-link active tab-button-style" id="nav-home-tab" data-bs-toggle="tab"
+                    //             data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home"
+                    //             aria-selected="true">Africa</button>
+                    //         <button class="nav-link" id="nav-asia" data-bs-toggle="tab" data-bs-target="#nav-profile"
+                    //             type="button" role="tab" aria-controls="nav-asia" aria-selected="false">Asia</button>
+                    //         <button class="nav-link" id="nav-north-america" data-bs-toggle="tab" data-bs-target="#nav-contact"
+                    //             type="button" role="tab" aria-controls="nav-north-america" aria-selected="false">North
+                    //             America</button>
+                    //         <button class="nav-link" id="nav-australia" data-bs-toggle="tab" data-bs-target="#nav-contact"
+                    //             type="button" role="tab" aria-controls="nav-australia"
+                    //             aria-selected="false">Australia</button>
+                    //         <button class="nav-link" id="nav-europe" data-bs-toggle="tab" data-bs-target="#nav-contact"
+                    //             type="button" role="tab" aria-controls="nav-europe" aria-selected="false">Europe</button>
+                    //         <button class="nav-link" id="nav-south-america" data-bs-toggle="tab" data-bs-target="#nav-contact"
+                    //             type="button" role="tab" aria-controls="nav-south-america" aria-selected="false">South
+                    //             America</button>
+                    //     </div>
+                    // </nav>
